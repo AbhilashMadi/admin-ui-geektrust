75fe6021387ab7c0242dab7d07982115aee05d14
AbhilashMadi <madiabhilash1563@gmail.com>
2024-10-28 15:12:18 +0530

    added credit card

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,7 +1,21 @@
 import { type FC } from "react";
+import { Link } from "react-router-dom";
 
 const Footer: FC = () => {
-  return (<footer />)
+  return (<footer className="flex justify-end flex-col text-sm">
+    {/* UI Credit Card */}
+    <div className="p-2 shadow-md border absolute top-4 right-4 rounded">
+      <p>
+        Repo:{" "}
+        <Link to={"https://github.com/AbhilashMadi/admin-ui-geektrust"} className="text-primary hover:underline underline-offset-4 decoration-wavy">
+          Admin Ui &#8599;
+        </Link>
+      </p>
+      <p className="">
+        Build by:{" "}<Link to={"https://github.com/AbhilashMadi"} className="text-primary hover:underline underline-offset-4 decoration-wavy">@Madi Abhilash &#8599;</Link>
+      </p>
+    </div>
+  </footer>)
 }
 
 export default Footer;
